24965399fb2594a66ccc8abd465f6d4f247f8b1e
khanhnam99 <khanhnam99@gmail.com>
2021-11-19 21:18:06 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import faker from "faker";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const person = {
+    id: 1,
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+  };
+
+  return <div>create a new component </div>;
 }
 
 export default App;
