18535de86ec4d149fdeb62d940ac88f3f98bd45c
khanhnam99 <khanhnam99@gmail.com>
2021-12-30 08:18:34 +0700

    post

--- src/containers/Main/Post/Post.js
+++ src/containers/Main/Post/Post.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import parse from "html-react-parser";
 import { getAll } from "../../../actions/postActions";
-import ReactPaginate from "react-paginate";
-//import Loading from "../../../components/common/Loading";
-//import ListPost from "../../../components/Post/ListPost";
-//import Pagination from "../../../components/common/Pagination";
-import Skeleton from "react-loading-skeleton";
+import Loading from "../../../components/common/Loading";
+import ListPost from "../../../components/Post/ListPost";
+import Pagination from "../../../components/common/Pagination";
 
 export default function Post() {
   const dataPost = useSelector((state) => state.postReducer);
@@ -30,16 +27,7 @@ export default function Post() {
   const { post, error, loading } = dataPost;
 
   if (loading || (post && post.items == null)) {
-    return (
-      <section className="py-5 text-center container">
-        <div className="row py-lg-5">
-          <div className="col-lg-6 col-md-8 mx-auto">
-            <Skeleton height={5} count={5} width={600} />
-          </div>
-        </div>
-      </section>
-    );
-    //return <Loading />;
+    return <Loading />;
   }
 
   let content = "";
@@ -48,20 +36,7 @@ export default function Post() {
   if (post && error === 0) {
     perPage = post.perPage;
     totalPage = Math.ceil(post.total / perPage);
-    content = (
-      <>
-        {post.items &&
-          post.items.map((item) => (
-            <div key={item.id}>
-              <div className="alert alert-primary" role="alert">
-                {item.name}
-              </div>
-            </div>
-          ))}
-      </>
-    );
-
-    //content = <ListPost dataPost={post.items} />;
+    content = <ListPost dataPost={post.items} />;
   }
 
   return (
@@ -69,33 +44,11 @@ export default function Post() {
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto"></div>
         {content}
-
-        <ReactPaginate
-          previousLabel="Previous"
-          nextLabel="Next"
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          previousLinkClassName="page-link"
-          nextClassName="page-item"
-          nextLinkClassName="page-link"
-          breakLabel="..."
-          breakClassName="page-item"
-          breakLinkClassName="page-link"
-          pageCount={totalPage}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={3}
-          onPageChange={handlePageChange}
-          containerClassName="pagination"
-          activeClassName="active"
-          forcePage={pageOffset}
-        />
-
-        {/* <Pagination
+        <Pagination
           totalPage={totalPage}
           handlePageChange={handlePageChange}
           pageOffset={pageOffset}
-        /> */}
+        />
       </div>
     </section>
   );
