00e50efbaf32cbf559e38206650d7e22b13740a0
khanhnam99 <khanhnam99@gmail.com>
2022-01-07 10:41:34 +0700

    Update

--- src/components/Post/ListPost.js
+++ src/components/Post/ListPost.js
@@ -1,12 +1,24 @@
 export default function ListPost({ dataPost }) {
   return (
-    <div>
+    <div className="container">
       {dataPost &&
         dataPost.map((item) => (
-          <div key={item.id}>
-            <div className="alert alert-primary" role="alert">
-              {item.name}
+          <div key={item.id} className="row mb-3">
+            <div className="col">{item.name}</div>
+            <div className="col">
+              <button type="button" className="btn btn-primary me-md-2">
+                <i className="bi bi-binoculars-fill"></i> Xem 1
+              </button>
+
+              <button type="button" className="btn btn-success me-md-2">
+                <i className="bi bi-binoculars"></i> Xem 2
+              </button>
+
+              <button type="button" className="btn btn-outline-danger">
+                <i className="bi-film"></i> Xem 3
+              </button>
             </div>
+            <div className="col">Column</div>
           </div>
         ))}
     </div>
